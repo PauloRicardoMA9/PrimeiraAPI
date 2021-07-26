68e3938c0bbedc9ec5b42d5cbf1bb1e508ce3b38
Paulo Ricardo <paulo.silva@ma9.com.br>
2021-07-26 14:28:02 -0300

    Finalizado CRUD de produtos

--- src/ma9.Api/Startup.cs
+++ src/ma9.Api/Startup.cs
@@ -34,7 +34,6 @@ namespace ma9.Api
             services.ResolveDependencies();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
